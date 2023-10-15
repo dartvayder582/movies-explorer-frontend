e9bad0f8549e4128508d2a90410d146e081b4977
Ivan Akimov <dartvayder582@yandex.ru>
2023-10-15 19:24:29 +0300

    подключить основные пакеты

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
