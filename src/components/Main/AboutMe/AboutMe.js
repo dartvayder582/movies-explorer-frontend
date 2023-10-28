5b9251dd6411fba77a33975c1d785dd7faee6293
Ivan Akimov <dartvayder582@yandex.ru>
2023-10-29 00:08:42 +0300

    исправить описание секции о студенте

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -10,20 +10,17 @@ const AboutMe = () => {
       <article className='student__info'>
         <div className='student__text-block'>
           <h3 className='student__title'>Иван</h3>
-          <p className='student__subtitle'>Фронтенд-разработчик, 30 лет</p>
+          <p className='student__subtitle'>Веб-разработчик, 26 лет</p>
           <p className='student__text'>
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
-            С 2015 года работал в компании «СКБ Контур».
-            После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+            Я живу в Ростове-на-Дону, закончил исторический факультет ЮФУ. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
+            С конца 2022 года начал свое обучение в Яндекс Практикуме.
+            После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с офисной работы.
           </p>
           <a href='https://github.com/dartvayder582' target='_blank' className='student__link link-style' rel="noreferrer">Github</a>
         </div>
         <img className='student__avatar' src={studentAvatar} alt='Фото студента'></img>
       </article>
       <Portfolio />
-
-
 
     </section>
   )
